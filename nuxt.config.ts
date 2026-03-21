3c3f3a4ed44bd12bed1eaa3c0ea67b5eae5049ab
jdchvz <jdchavez@cambridge.org>
2026-03-21 11:21:16 +0800

    fix design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
+  css: ['~/assets/css/main.css'], // make sure this exists
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   nitro: {
     preset: 'node-server'
   },
   runtimeConfig: {
     public: {
-      socketUrl: ''  // empty = same host
+      socketUrl: ''
     }
   }
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './components/**/*.{vue,js,ts}',
     './layouts/**/*.vue',
-    './pages/**/*.{vue,js,ts}',
+    './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './stores/**/*.{js,ts}',
-    './app.vue'
+    './app.vue',
   ],
   theme: {
-    extend: {
-      colors: {
-        navy: {
-          900: '#0f1e33',
-          800: '#132a49',
-          700: '#1E3557'
-        }
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif']
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
